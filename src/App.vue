f946b25c98154a08c4ed59026de2200a556befe1
zhansito <zhansito@gmail.com>
2021-11-25 21:49:00 +0600

    continue final

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ export default {
   data: () => ({
         isDefinition: false,
         word: '',
-        definition: '',
+        definition: null,
     }),
     mounted() {
         this.loadDef()
@@ -18,9 +18,21 @@ export default {
             http.get(`words/${this.word}/definitions`)
             .then(response => {
                 console.log(response.data);
-                this.definition = response.data
+                this.definition = response.data.definitions
             })
             .finally(() => { this.isDefinition = false })
+        },
+        loadSyn(){
+          this.isDefinition = true
+            
+
+            http.get(`words/${this.word}/synonyms`)
+            .then(response => {
+                console.log(response.data);
+                this.definition = response.data
+            })
+            .finally(() => { this.isDefinition = false })      
+            
         }
   //   } 
   // methods:{
@@ -43,15 +55,16 @@ export default {
 
     </v-container>
     <v-btn @click="loadDef" style="margin-right: 5px">Definition</v-btn>
-    <v-btn @click="loadDef" style="margin-right: 5px">Synonyms</v-btn>
+    <v-btn @click="loadSyn" style="margin-right: 5px">Synonyms</v-btn>
 
     <v-text-field v-model="word" label="Enter the word"/>
 
         <v-card>
           <v-list >
-            <v-list-item>
-              <v-list-item-content>
-              {{ definition }}
+            <v-list-item v-for="(item, i) in definition" :key="i">
+              <v-list-item-content >
+              <!-- {{ definition[0] }} -->
+              {{ item.definition}}
               </v-list-item-content>
             </v-list-item>
           </v-list>
